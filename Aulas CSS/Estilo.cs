b4c4b20eecabee922215a956c15f5f9d86206a6d
Cab18 <anacarolinanascimentobrito@gmail.com>
2021-09-17 23:03:40 -0300

    Exercicios feitos até aqui não em ordem

---
+++ Aulas CSS/Estilo.cs
@@ -0,0 +1,52 @@
+   
+
+input[type="submit"] {   
+     font-weight: bold; /* texto em negrito */ 
+     color:lawngreen;
+     border-radius: 10cm;
+     margin: 10px auto;
+     } 
+
+     input[type="text"] {   
+     font-weight: bold; /* texto em negrito */ 
+     color:rgb(73, 139, 19);
+     border-color: rgb(11, 79, 146);
+     border: 30px;
+     border-radius: 20cm;
+     }
+            #box {
+                width: 100px;
+                height: 100px;
+                background-color:white;
+                 box-shadow: 10px 10px 8px 10px #888888;
+                float: right;
+            }
+
+            #formatacao2{
+                color:blue;
+                border: 23px solid;
+
+                #box:hover{
+                    background-color:yellow;
+                    width:110px;
+                    height:110px;
+
+            }
+
+            h1, ul, li{
+                color:blueviolet;
+            }
+            ul, li{
+                color:red;
+            }
+            p{
+                font-size:30px;
+                text-align:justify;
+                color:chocolate;
+                font-weight:bold;
+                font-family: sans-serif;                
+            }
+            .todosinputs{
+                background-color:tomato;
+                padding: 0 auto;
+                }    
